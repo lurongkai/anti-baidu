4a794ece07d350ef710135db1dda515a7e98b5cb
Lu Rongkai <lurongkai@gmail.com>
2013-03-25 12:25:46 +0800

    format code.

--- js/loader.js
+++ js/loader.js
@@ -14,46 +14,46 @@
 	    	.fail(function(){});
 	};
 	
-  var loader = function(){
-    // Only do anything if jQuery isn't defined
-    if (typeof jQuery === 'undefined') {
-      function getScript(url, success) {
-        var script = document.createElement('script');
-        script.src = url;
+	var loader = function(){
+		// Only do anything if jQuery isn't defined
+		if (typeof jQuery === 'undefined') {
+			function getScript(url, success) {
+				var script = document.createElement('script');
+				script.src = url;
 
-        var head = document.getElementsByTagName('head')[0],
-        done = false;
-        // Attach handlers for all browsers
-        script.onload = script.onreadystatechange = function() {
-          if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
-            done = true;
-            // callback function provided as param
-            success();
-            script.onload = script.onreadystatechange = null;
-            head.removeChild(script);
-          };
-        };
-        head.appendChild(script);
-      };
+				var head = document.getElementsByTagName('head')[0],
+				done = false;
+				// Attach handlers for all browsers
+				script.onload = script.onreadystatechange = function() {
+					if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+						done = true;
+						// callback function provided as param
+						success();
+						script.onload = script.onreadystatechange = null;
+						head.removeChild(script);
+					};
+				};
+				head.appendChild(script);
+			};
 
-      getScript(jQuery_cdn, function() {
-        if (typeof jQuery=='undefined') { // Super failsafe - still somehow failed...
-          return;
-        } else {
-          anti_baidu();
-        }
-      });
-    } else {
-      // jQuery was already loaded
-      anti_baidu();
-    };
-  }
+			getScript(jQuery_cdn, function() {
+				if (typeof jQuery=='undefined') { // Super failsafe - still somehow failed...
+					return;
+				} else {
+					anti_baidu();
+				}
+			});
+		} else {
+		  // jQuery was already loaded
+		  anti_baidu();
+		};
+	}
 	
-  var url=document.referrer;
-  if (url && url.search("http://")>-1) {
-      var refurl =  url.match(/:\/\/(.[^/]+)/)[1];
-      if(refurl.indexOf("baidu.com") > -1){
-        loader();
-      }
-  }
+	var url=document.referrer;
+	if (url && url.search("http://")>-1) {
+		var refurl =  url.match(/:\/\/(.[^/]+)/)[1];
+		if(refurl.indexOf("baidu.com") > -1){
+			loader();
+		}
+	}
 })();
