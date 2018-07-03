a746c06b0d8d699942617d7b5e533ff0223ecd9b
Hao Chen <haoel@hotmail.com>
2018-07-03 22:46:30 +0800

    modify the url to use the latest version

--- js/anti-baidu-latest.js
+++ js/anti-baidu-latest.js
@@ -25,7 +25,7 @@
                 .done(function () {
                     (function ($) {
                         $(document).ready(function () {
-                            var antiDiv = '<div id="nobaidu_dlg" style="background-color:#fff; border-radius:15px;color:#000;display:none;padding:20px;min-width:450px;min-height:180px;"><img src="https://cdn.jsdelivr.net/gh/haoel/anti-baidu@0.5-release/images/nobaidu.jpg" align="left"><p style="margin-left:200px;margin-top: 20px; line-height: 30px;">检测到你还在使用百度这个搜索引擎，<br>作为一个程序员，这是一种自暴自弃！<br><br></p><p align="center" style="margin-top:20px;"><b><a href="http://coolshell.cn/articles/9308.html">做环保的程序员，从不用百度开始！</a></b></p></div>';
+                            var antiDiv = '<div id="nobaidu_dlg" style="background-color:#fff; border-radius:15px;color:#000;display:none;padding:20px;min-width:450px;min-height:180px;"><img src="https://cdn.jsdelivr.net/gh/haoel/anti-baidu/images/nobaidu.jpg" align="left"><p style="margin-left:200px;margin-top: 20px; line-height: 30px;">检测到你还在使用百度这个搜索引擎，<br>作为一个程序员，这是一种自暴自弃！<br><br></p><p align="center" style="margin-top:20px;"><b><a href="http://coolshell.cn/articles/9308.html">做环保的程序员，从不用百度开始！</a></b></p></div>';
                             $("body").append(antiDiv);
                             if (isBaiduReferrer) {
                                 $('#nobaidu_dlg').bPopup();
