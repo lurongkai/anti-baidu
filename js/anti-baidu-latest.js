e4895341f67895628d00fb58854f6493e7239b8c
Hao Chen <haoel@hotmail.com>
2018-07-03 22:35:49 +0800

    1)change the url by using cdn.jsdelivr.net, 2) adding the js minify script, 3) reformat the source map file, 4) refactory the test page

--- js/anti-baidu-latest.js
+++ js/anti-baidu-latest.js
@@ -1,11 +1,15 @@
 // Author: http://weibo.com/fanweixiao
 
 (function () {
-    var jQuery_cdn = "https://cdn.bootcss.com/jquery/1.12.4/jquery.js"; 
-    var jQuery_bpopup_cdn = "https://cdn.bootcss.com/bPopup/0.11.0/jquery.bpopup.min.js"; 
+    var jQuery_cdn = "https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js";
+    var jQuery_bpopup_cdn = "https://cdn.jsdelivr.net/gh/dinbror/bpopup@0.11.0/jquery.bpopup.min.js";
 
     var isBaiduReferrer = (function () {
         var url=document.referrer;
+	if ( isTest === true ) {
+	    url = TestURL;
+	}
+
         if (url && (url.search("http[s]?://") > -1 )) {
             var refurl = url.match(/:\/\/(.[^/]+)/)[1];
             if (refurl.indexOf("baidu.com") > -1) {
@@ -21,7 +25,7 @@
                 .done(function () {
                     (function ($) {
                         $(document).ready(function () {
-                            var antiDiv = '<div id="nobaidu_dlg" style="background-color:#fff; border-radius:15px;color:#000;display:none;padding:20px;min-width:450px;min-height:180px;"><img src="http://lurongkai.github.com/anti-baidu/images/nobaidu.jpg" align="left"><p style="margin-left:200px;margin-top: 20px; line-height: 30px;">检测到你还在使用百度这个搜索引擎，<br>作为一个程序员，这是一种自暴自弃！<br><br></p><p align="center" style="margin-top:20px;"><b><a href="http://coolshell.cn/articles/9308.html">做环保的程序员，从不用百度开始！</a></b></p></div>';
+                            var antiDiv = '<div id="nobaidu_dlg" style="background-color:#fff; border-radius:15px;color:#000;display:none;padding:20px;min-width:450px;min-height:180px;"><img src="https://cdn.jsdelivr.net/gh/haoel/anti-baidu@0.5-release/images/nobaidu.jpg" align="left"><p style="margin-left:200px;margin-top: 20px; line-height: 30px;">检测到你还在使用百度这个搜索引擎，<br>作为一个程序员，这是一种自暴自弃！<br><br></p><p align="center" style="margin-top:20px;"><b><a href="http://coolshell.cn/articles/9308.html">做环保的程序员，从不用百度开始！</a></b></p></div>';
                             $("body").append(antiDiv);
                             if (isBaiduReferrer) {
                                 $('#nobaidu_dlg').bPopup();
